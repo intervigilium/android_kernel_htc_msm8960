50d2419716ccb5db2bb891ff2ad89389254ee53d
Ethan Chen <intervigil@gmail.com>
2012-11-11 16:07:24 -0800

    Revert "msm_fb: display: disable waiting for mdp4 writeback on HTC 8960"
    This reverts commit 5145a4810815e0a012841895303a48dc1d2a47b2.

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -863,20 +863,9 @@ int mdp4_overlay_borderfill_supported(void);
 int mdp4_overlay_writeback_on(struct platform_device *pdev);
 int mdp4_overlay_writeback_off(struct platform_device *pdev);
 void mdp4_writeback_overlay(struct msm_fb_data_type *mfd);
-#if !defined(CONFIG_MACH_ELITE) && !defined(CONFIG_MACH_JET) && !defined(CONFIG_MACH_VILLE)
 void mdp4_writeback_kickoff_video(struct msm_fb_data_type *mfd,
 		struct mdp4_overlay_pipe *pipe);
 void mdp4_writeback_dma_busy_wait(struct msm_fb_data_type *mfd);
-#else
-/* HTC doesn't include mdp4_overlay_writeback, don't support mdp writeback */
-static inline void mdp4_writeback_kickoff_video(struct msm_fb_data_type *mfd,
-		struct mdp4_overlay_pipe *pipe)
-{
-}
-static inline void mdp4_writeback_dma_busy_wait(struct msm_fb_data_type *mfd)
-{
-}
-#endif
 void mdp4_overlay1_done_writeback(struct mdp_dma_data *dma);
 void mdp4_dma_e_done_dtv(void);
 
