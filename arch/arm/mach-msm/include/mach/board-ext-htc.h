8c48cbd164f73dbf5449e81e33dd8cf8c4af7c8f
Ethan Chen <intervigil@gmail.com>
2013-09-08 16:50:37 -0700

    msm: HTC: unconditionally include camera structures
    Change-Id: I8b4c6329438597359f444d24de7fb29c67131922

--- arch/arm/mach-msm/include/mach/board-ext-htc.h
+++ arch/arm/mach-msm/include/mach/board-ext-htc.h
@@ -106,7 +106,6 @@ typedef struct {
 } mhl_driving_params;
 #endif
 
-#ifdef CONFIG_MSM_CAMERA
 enum msm_camera_csi_data_format {
 	CSI_8BIT,
 	CSI_10BIT,
@@ -201,6 +200,5 @@ enum cam_vcm_onoff_type {
 	STATUS_OFF,
 	STATUS_ON,
 };
-#endif /* CONFIG_MSM_CAMERA */
 
 #endif /* __ASM_ARCH_MSM_BOARD_EXT_HTC_H */
